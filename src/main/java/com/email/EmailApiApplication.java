a24d555dd763465f18fb563325cb90e0e9ef0996
savani vivek <savani.vivek3@gmail.com>
2022-08-06 22:22:56 +0530

    final commit

--- src/main/java/com/email/EmailApiApplication.java
+++ src/main/java/com/email/EmailApiApplication.java
@@ -2,9 +2,16 @@ package com.email;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class EmailApiApplication {
+public class EmailApiApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(EmailApiApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(EmailApiApplication.class, args);
